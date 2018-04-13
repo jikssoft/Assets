e357d99efe0b7d67b4b7e97bfdc1c7169940ddc1
jikssoft <ikssoft@hotmail.com>
2018-04-13 23:28:15 +0900

    ios game center 적용
    ios game center 적용

--- Script/LeaderBoardManager.cs
+++ Script/LeaderBoardManager.cs
@@ -6,7 +6,21 @@ public class LeaderBoardManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		if (RuntimePlatform.IPhonePlayer == Application.platform) {
+			
+			GameCenterManager.OnAuthFinished += GameCenterManager_OnAuthFinished;
+			GameCenterManager.Init ();
+
+		}
+	}
+
+	void GameCenterManager_OnAuthFinished (SA.Common.Models.Result obj)
+	{
+		if (obj.IsSucceeded) {
+			Debug.Log ("GameCenter Init");
+		} else {
+			Debug.Log ("GameCenter Init fail");
+		}
 	}
 	
 	// Update is called once per frame
@@ -14,9 +28,15 @@ public class LeaderBoardManager : MonoBehaviour {
 		
 	}
 
+
     static string best_level_id = "CgkI58npnusaEAIQAQ";
     static string best_score_infinity_id = "CgkI58npnusaEAIQAg";
     static string best_score_hell_id = "CgkI58npnusaEAIQAw";
+
+	static string best_level_id_ios = "com.taptapdrill.leaderboard.levelranking";
+	static string best_score_infinity_id_ios = "com.taptapdrill.leaderboard.infinityranking";
+	static string best_score_hell_id_ios = "com.taptapdrill.leaderboard.hellranking";
+
 
     public static void LoadLeaderboard()
     {
@@ -38,33 +58,72 @@ public class LeaderBoardManager : MonoBehaviour {
     }
 
     public static void UpdateBestLevel(int level)
-    {
-        GooglePlayManager.Instance.SubmitScoreById(best_level_id, level);
-    }
+	{
+		if (RuntimePlatform.Android == Application.platform) {
+			GooglePlayManager.Instance.SubmitScoreById (best_level_id, level);
+    
+		} else {
+			GameCenterManager.OnScoreSubmitted += OnScoreSubmitted;
+			GameCenterManager.ReportScore(level, best_level_id_ios); 
+		}
+	}
+
+	static void OnScoreSubmitted(GK_LeaderboardResult result)
+	{
+		GameCenterManager.OnScoreSubmitted -= OnScoreSubmitted;
+		if (result.IsSucceeded) {
+			Debug.Log ("succeeded submit score");
+		}
+	}
 
     public static void UpdateBestScoreInfinityMode(int score)
     {
-        GooglePlayManager.Instance.SubmitScoreById(best_score_infinity_id, score);
+        
+		if (RuntimePlatform.Android == Application.platform) {
+			GooglePlayManager.Instance.SubmitScoreById(best_score_infinity_id, score);
+
+		} else {
+			GameCenterManager.OnScoreSubmitted += OnScoreSubmitted;
+			GameCenterManager.ReportScore (score, best_score_infinity_id_ios); 
+		}
     }
 
     public static void UpdateBestScoreHellMode(int score)
     {
-        GooglePlayManager.Instance.SubmitScoreById(best_score_hell_id, score);
+		if (RuntimePlatform.Android == Application.platform) {
+			GooglePlayManager.Instance.SubmitScoreById(best_score_hell_id, score);
+
+		} else {
+			GameCenterManager.OnScoreSubmitted += OnScoreSubmitted;
+			GameCenterManager.ReportScore (score, best_score_hell_id_ios); 
+		}
     }
 
     public static void ShowBestLevel()
     {
-        GooglePlayManager.Instance.ShowLeaderBoardById (best_level_id);
+		if (RuntimePlatform.Android == Application.platform) {
+			GooglePlayManager.Instance.ShowLeaderBoardById (best_level_id);
+		} else {
+			GameCenterManager.ShowLeaderboard (best_level_id_ios);
+		}
     }
 
     public static void ShowBestScoreInfinityMode()
     {
-        GooglePlayManager.Instance.ShowLeaderBoardById(best_score_infinity_id);
+		if (RuntimePlatform.Android == Application.platform) {
+			GooglePlayManager.Instance.ShowLeaderBoardById (best_score_infinity_id);
+		} else {
+			GameCenterManager.ShowLeaderboard (best_score_infinity_id_ios);
+		}
     }
 
     public static void ShowBestScoreHellMode()
     {
-        GooglePlayManager.Instance.ShowLeaderBoardById(best_score_hell_id);
+		if (RuntimePlatform.Android == Application.platform) {
+			GooglePlayManager.Instance.ShowLeaderBoardById (best_score_hell_id);
+		} else {
+			GameCenterManager.ShowLeaderboard (best_score_hell_id_ios);
+		}
     }
 
 }
